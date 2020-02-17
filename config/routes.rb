05f695363a7bb6911f10eca522824387b0047d10
Dennis Neely <dennis.neely22@gmail.com>
2020-02-17 17:27:43 +0100

    added routes for tractors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tractors#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tractors, only: %i[show new create]
 end
