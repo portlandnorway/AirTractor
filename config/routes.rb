ecbc29665e651ad56e79c893c7d61d5c4847aafd
Dennis Neely <dennis.neely22@gmail.com>
2020-02-17 17:36:02 +0100

    no change made

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tractors#index'
 
-  resources :tractors, only: %i[show new create]
+  resources :tractors, only: %i[show new create] do
+
 end
