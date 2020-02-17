86f39910c20119098db15d803ad138243ab9300c
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-17 18:43:09 +0100

    Added seeds and some details on the view. The view needs tractors to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 100 fake users...'
+100.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::PhoneNumber.subscriber_number(length: 8)
+  )
+  user.save!
+end
+
+puts 'Finished with the users!'
+
+b = (1..100).to_a.shuffle
+
+puts 'Creating 100 fake tractors...'
+100.times do
+  tractor = Tractor.new(
+    name: Faker::FunnyName.name,
+    details: Faker::Vehicle.car_options,
+    reward: Faker::Number.number(digits: 3),
+    user_id: b.pop,
+    address: Faker::Address.street_address
+  )
+  tractor.save!
+end
+
+puts 'Finished with the tractors!'
+
+a = (1..100).to_a.shuffle
+
+puts 'Creating 20 fake booking...'
+20.times do
+  booking = Booking.new(
+    tractor_id: a.pop,
+    user_id: rand(1..100)
+  )
+  booking.save!
+end
+
+puts 'Finished with the orders! Do you want a tractor also?'
