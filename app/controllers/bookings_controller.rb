3f019f75451fb3bc55a1d5ef1f5d3584c7488f6d
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-18 17:36:32 +0100

    The booking controller, view and route up and running

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  # def new
+  #   @tractor = Tractor.find(params[:tractor_id])
+  #   @booking = Booking.new
+  # end
+
+  def create
+    @booking = Booking.new(tractor_id: params[:tractor_id], user_id: current_user.id)
+    @booking.save
+    @tractor = Tractor.find(params[:tractor_id])
+    # Or should we redirect to a booking path or a profile page
+    redirect_to booking_path(@booking)
+  end
+
+  def show
+  end
+end
