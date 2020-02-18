5e00a77db74e4f059ec61ef4faa551faa415db3b
Dennis Neely <dennis.neely22@gmail.com>
2020-02-18 12:08:45 +0100

    tractors#show in progress

--- app/controllers/tractors_controller.rb
+++ app/controllers/tractors_controller.rb
@@ -1,5 +1,16 @@
 class TractorsController < ApplicationController
+  before_action :find_tractor, only: %i[show]
+
   def index
     @tractors = Tractor.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_tractor
+    @tractor = Tractor.find(params[:id])
   end
 end
