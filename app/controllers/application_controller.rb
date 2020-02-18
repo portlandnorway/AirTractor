25eaeb3d96a608ac8b00d9644e01095621005e2a
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-18 15:47:19 +0100

    Added the logo, changed the width of the form and added a primary-btn at the login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+  private
+  def layout_by_resource
+    if devise_controller?
+      "no_navbar_layout"
+    else
+      "application"
+    end
+  end
 end
