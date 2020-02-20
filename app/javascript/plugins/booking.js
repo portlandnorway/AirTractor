5a12b01e0080dc628262721090e6399f27af0291
Dennis Neely <dennis.neely22@gmail.com>
2020-02-20 18:06:32 +0100

    date fix hot fix my bad

--- app/javascript/plugins/booking.js
+++ app/javascript/plugins/booking.js
@@ -5,26 +5,26 @@ const toggleDateInputs = function() {
   const endDateInput = document.getElementById('booking_end_date');
 
   if (startDateInput && endDateInput) {
-    const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
-
+    const unavailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
+    console.log("Here are the", unavailableDates)
     flatpickr(startDateInput, {
-    minDate: 'today',
-    dateFormat: 'd-m-Y',
-    disable: unvailableDates,
-    onChange: function(selectedDates, selectedDate) {
-      if (selectedDate === '') {
-        endDateInput.disabled = true;
+      minDate: 'today',
+      dateFormat: 'Y-m-d',
+      disable: unavailableDates,
+      onChange: function(selectedDates, selectedDate) {
+        if (selectedDate === '') {
+          endDateInput.disabled = true;
+        }
+        let minDate = selectedDates[0];
+        minDate.setDate(minDate.getDate() + 1);
+        endDateCalendar.set('minDate', minDate);
+        endDateInput.disabled = false;
       }
-      let minDate = selectedDates[0];
-      minDate.setDate(minDate.getDate() + 1);
-      endDateCalendar.set('minDate', minDate);
-      endDateInput.disabled = false;
-    }
-  });
+    });
     const endDateCalendar =
       flatpickr(endDateInput, {
-        dateFormat: 'd-m-Y',
-        disable: unvailableDates,
+        dateFormat: 'Y-m-d',
+        disable: unavailableDates,
         });
   };
 };
