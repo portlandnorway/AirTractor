fc04f1b7b492dfd8b636f8dab137e3e1e0c3763b
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-20 11:01:59 +0100

    Some comments in the js file and css file for the scroll btn

--- app/javascript/plugins/init_scroll_btn.js
+++ app/javascript/plugins/init_scroll_btn.js
@@ -1,3 +1,5 @@
+// Copied from codepen https://codepen.io/nxworld/pen/OyRrGy (section 5)
+// Missing: to rewrite the code from jQuery:
 // Select the btn
 // Addeventlistner (click)
 // Another querySel.animate (might be Jquery). Try 'scrollTo' in MDN.
